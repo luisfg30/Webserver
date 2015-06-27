ce08424195724596924f2af593b37f26181faec9
luisfg30 <luisfg30@hotmail.com>
2015-06-27 15:58:03 -0300

    -Server Rodando em Paralelo com a interface grafica. -Pode abrir varios servers(um em cad athread). -Consegui passar o server como parametro para outras classes
    Signed-off-by: luisfg30 <luisfg30@hotmail.com>

--- Software/Server.py
+++ Software/Server.py
@@ -1,11 +1,38 @@
 import Conexao
-import http.server
-import socketserver
 import threading
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+class HTTPParser(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print("\nServer recebeu do browser: \n"+self.requestline+"\n"+str(self.headers))
+        index=self.server.servidor.procura_conexao(self.client_address[0],self.client_address[1],self.date_time_string(None))
+        print("\nINDEX: "+str(index))
+        if index == -1: # conexao ainda nao existe
+            self.server.servidor.nova_conexao(self.client_address[0],self.client_address[1],self.date_time_string(None))
+            self.server.servidor.nova_requisicao(len(self.server.servidor.get_lista())-1,len(str(self.headers)),self.command,self.path,self.request_version,self.server.servidor.get_hostname,self.date_time_string(None))
+        else: #conexao ja existe
+           self.server.servidor.nova_requisicao(index,len(str(self.headers)),self.command,self.path,self.request_version,self.server.servidor.get_hostname,self.date_time_string(None))
+        
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.server.servidor.print_conexoes()
+     
+class customServer(HTTPServer):
+        def __init__(self,servidor,server_address,parser):
+            super(customServer,self).__init__(server_address,parser)
+            self.servidor=servidor
+            
 class Server(object):
 
     def __init__(self,maxConexoes,porta,timeOut,download):
+        self.hostname="192.168.100.12"
         self.maxConexoes=maxConexoes
         self.porta=porta
         self.timeOut=timeOut
@@ -14,16 +41,41 @@ class Server(object):
         #print apenas para mostrar que recebeu os parametros, depois a construtora ja inicia o servidor
         print("\nPARAMETROS SERVER\n Max Conexoes:"+str(self.maxConexoes)+"\n Porta:"+str(self.porta)+"\n Time Out:"+str(self.timeOut)+"\n Enable Download:"+str(self.download))  
         
-        self.test_method()
         self.start_server()
-
-    def test_method(self):
-         c=Conexao.Conexao(8000,"192.168.1.1",True)
-         self.listaConexoes.append(c)
-         print("\n Lista de conexoes:")
-         for i in range(len(self.listaConexoes)):
-            print("\n " +self.listaConexoes[i].get_IP())  
-            
+        
+    def procura_conexao(self,IP,porta,data):
+        if len(self.listaConexoes)==0: #lista vazia
+            return -1
+        else:    
+            for i in range(len(self.listaConexoes)):
+                if self.listaConexoes[i].get_IP()==IP and self.listaConexoes[i].get_porta==porta and self.listaConexoes[i].get_data()==data:
+                    return i# retorna o indice da conexao
+                else:
+                    return -1 #retorna -1 se a conexao nao existe
+    
+    def print_conexoes(self):
+        for i in range(len(self.listaConexoes)):
+            print("\n ["+str(i)+"] ")
+            self.listaConexoes[i].print_self()
+    
+    def get_porta(self):
+        return self.porta
+    
+    def get_hostname(self):
+        return self.hostname
+    def get_conexao(self,index):
+        return self.listaConexoes[index]
+    
+    def get_lista(self):
+        return self.listaConexoes
+        
+    def nova_conexao(self,IP,porta,data):
+        c=Conexao.Conexao(IP,porta,data)
+        self.listaConexoes.append(c)
+        
+    def nova_requisicao(self,index,bytes,tipo,paginaAcessada,versaoProtocoloHTTP,hostname,data):
+        self.listaConexoes[index].nova_requisicao(bytes,tipo,paginaAcessada,versaoProtocoloHTTP,hostname,data)
+        
     def restart_server(self):
         
             if len(self.listaConexoes)==0:
@@ -41,10 +93,11 @@ class Server(object):
         self.download=download
         print("\n PARAMETROS SERVER\n Max Conexoes:"+str(self.maxConexoes)+"\n Porta:"+str(self.porta)+"\n Time Out:"+str(self.timeOut)+"\n Enable Download:"+str(self.download))
     def start_server(self):
-        self.mt= mainThread(self.maxConexoes,self.porta,self.timeOut,self.download)
+        self.mt= mainThread(self,self.maxConexoes,self.porta,self.timeOut,self.download)
 
 class mainThread(threading.Thread):
-    def __init__(self,maxConexoes,porta,timeOut,download):
+    def __init__(self,servidor,maxConexoes,porta,timeOut,download):
+        self.servidor=servidor
         self.porta=porta
         self.timeOut=timeOut
         self.download=download
@@ -52,9 +105,7 @@ class mainThread(threading.Thread):
         self.start()
         
     def run(self):
-        self.Handler = http.server.SimpleHTTPRequestHandler
-
-        self.httpd = socketserver.TCPServer(("", self.porta), self.Handler)
-
-        print("serving at port", self.porta)
-        self.httpd.serve_forever()
+        server_address=(self.servidor.hostname, self.porta)
+        myServer = customServer(self.servidor,server_address, HTTPParser)
+        print(time.asctime(), "Server Starts - %s:%s" % (self.servidor.hostname, self.porta))
+        myServer.serve_forever()
